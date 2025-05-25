33922a5ef98cec7f546f7f791e4215e5b5010b11
Marco Josefsen <majos6959@oslomet.no>
2025-05-25 15:22:19 +0200

    Initial commit

---
+++ Project Paws/Project_PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_PawsApp.swift
+//  Project Paws
+//
+//  Created by Marco Josefsen on 25/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Project_PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Project PawsTests/Project_PawsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_PawsTests.swift
+//  Project PawsTests
+//
+//  Created by Marco Josefsen on 25/05/2025.
+//
+
+import Testing
+@testable import Project_Paws
+
+struct Project_PawsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
